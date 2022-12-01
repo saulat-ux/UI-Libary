922b15395285b46c55738ddf7ae70f34e9673844
DanyalSajidCofares <dsajid@externos.cofares.es>
2022-12-01 16:06:00 +0500

    made tge dropdown componet today

--- src/My-Ui/tooltip.js
+++ src/My-Ui/tooltip.js
@@ -1,10 +1,11 @@
 import './style/tooltip.css';
 class Tooltip {
     constructor(element){
-        this.element= element;
+       this.element= element;
         this.message= element.getAttribute('data-message');
     }
     init(){
+
         // this is going to be the popup tooltip
         const tip = document.createElement('div');
         tip.classList.add('tip');
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import Tooltip from './My-Ui/tooltip'
+import Dropdown from './My-Ui/dropdown';
+
 
 const toolTip = new Tooltip(document.querySelector('.tooltip'));
+console.log(toolTip);
 toolTip.init();
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown)
+    instance.init();
+})
